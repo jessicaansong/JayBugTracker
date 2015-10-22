b14e8bffde1b2cfa2326ef9d74ef592ea7028978
jessicaansong <jessica.ansong@gmail.com>
2015-10-22 14:30:51 -0400

    Second Commit

---
+++ Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace JayBugTracker.Models
+{
+    public class Admin
+    {
+        public string User { get; set; }
+        public MultiSelectList Role { get; set; }
+        public string[] SelectedRoles { get; set; }
+    }
+
+}
